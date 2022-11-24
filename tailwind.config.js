8be37bebcd60d2b40f55e7803504c6c4bf265c2b
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2022-11-23 20:30:05 -0500

    Apply Tailwind CSS to page layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'tablet': '576px',
+      // => @media (min-width:576px) { ... }
+      'desktop': '992px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   plugins: [],
